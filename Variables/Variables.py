c121a3fba9ef6f2058b1647d42b44bc97485449e
Jeancarlos <Jeancarlosdev83@gmail.com>
2026-09-07 19:40:20 -0400

    Add variable types exercises

---
+++ Variables/Variables.py
@@ -0,0 +1,61 @@
+# ejercicios de tipos de variables
+
+
+# ejercicio 1
+# guardar mi nombre y mi ciudad
+
+nombre = "Jeancarlos"
+ciudad = "Santo Domingo"
+
+print(nombre)
+print(ciudad)
+
+
+# ejercicio 2
+# guardar mi edad
+
+edad = 20
+
+print(edad)
+print(type(edad))
+
+
+# ejercicio 3
+# guardar un precio con decimales
+
+precio = 25.50
+
+print(precio)
+print(type(precio))
+
+
+# ejercicio 4
+# saber si estoy estudiando python
+
+estudiando_python = True
+
+print(estudiando_python)
+print(type(estudiando_python))
+
+
+# ejercicio 5
+# crear diferentes tipos de variables
+
+nombre = "Jeancarlos"
+edad = 20
+altura = 1.75
+estudiando = True
+
+print(nombre)
+print(edad)
+print(altura)
+print(estudiando)
+
+
+# ejercicio 6
+# ver que tipo de dato es cada variable
+
+print(type(nombre))
+print(type(edad))
+print(type(altura))
+print(type(estudiando))
