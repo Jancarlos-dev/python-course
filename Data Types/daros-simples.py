3350d501f770afece0ad77275372fce165a5da9d
Jeancarlos <Jeancarlosdev83@gmail.com>
2026-09-07 15:58:29 -0400

    Complete first session

---
+++ Data Types/daros-simples.py
@@ -0,0 +1,34 @@
+# tipo de datos de texto o cadena de texto
+
+# son esto  
+
+# "strings" # cadena de texto
+
+# 'strings' # cadena de texto
+
+# '''strings''' # cadena de texto
+
+# """strings""" # cadena de texto
+
+"""tus datos son:
+nombre: jan carlos
+apellido: garcias
+edad: 20
+correo:chuponfs 123
+ciudad: santo domingo oeste
+"""
+
+# tipo de datos numericos
+
+# int #numeros enteros 40
+
+# float #numeros con coma 40.2
+
+# boolean # true o false
+
+
+
+40
+
+40.2
+
